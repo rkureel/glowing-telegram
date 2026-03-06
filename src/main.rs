5ad7f80251b75949c1ac524dc8d7f62101a6bc46
Rahul Kureel <r.dev2000@gmail.com>
2026-03-05 19:44:26 -0800

    feat: setup routing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use std::{convert::Infallible, error::Error, net::SocketAddr};
+use std::{error::Error, net::SocketAddr};
 
-use http_body_util::Full;
-use hyper::{Request, Response, body::Bytes, server::conn::http1, service::service_fn};
+use http_body_util::BodyExt;
+use http_body_util::{Empty, Full, combinators::BoxBody};
+use hyper::{Request, Response, StatusCode, body::Bytes, server::conn::http1, service::service_fn};
 use hyper_util::rt::TokioIo;
 use tokio::net::TcpListener;
-
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
@@ -15,7 +15,7 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
         let io = TokioIo::new(stream);
         tokio::task::spawn(async move {
             if let Err(err) = http1::Builder::new()
-                .serve_connection(io, service_fn(hello))
+                .serve_connection(io, service_fn(handle))
                 .await
             {
                 eprintln!("Error serving connection: {:?}", err);
@@ -24,6 +24,37 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     }
 }
 
-async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
-    Ok(Response::new(Full::new(Bytes::from("Hello!"))))
+async fn handle(
+    request: Request<hyper::body::Incoming>,
+) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
+    match request.uri().path() {
+        "/" => hello(request).await,
+        _ => not_found(request).await,
+    }
+}
+
+async fn hello(
+    _: Request<hyper::body::Incoming>,
+) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
+    Ok(Response::new(full("Hello")))
+}
+
+async fn not_found(
+    _: Request<hyper::body::Incoming>,
+) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
+    let mut not_found = Response::new(empty());
+    *not_found.status_mut() = StatusCode::NOT_FOUND;
+    Ok(not_found)
+}
+
+fn empty() -> BoxBody<Bytes, hyper::Error> {
+    Empty::<Bytes>::new()
+        .map_err(|never| match never {})
+        .boxed()
+}
+
+fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
+    Full::new(chunk.into())
+        .map_err(|never| match never {})
+        .boxed()
 }
